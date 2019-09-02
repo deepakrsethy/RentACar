fa343562d2a76f1e46b176ef58e0f720a774ff0c
ds10805 <SethyDeepakRanjan@JohnDeere.com>
2019-09-02 16:50:10 +0530

    Added test

--- src/test/java/com/deepak/rentACar/controller/SearchCarControllerTest.java
+++ src/test/java/com/deepak/rentACar/controller/SearchCarControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import junit.framework.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,7 +34,10 @@ public class SearchCarControllerTest {
 	public void testSearchURI() throws Exception{
 		mockMvc.perform(get("rentACar/search/Pune")).andExpect(status().isOk());
 	}*/
-	
+	@Test
+	public void testSearch() throws Exception{
+		Assert.assertNotNull(controller.searchCarsInACity(""));
+	}
 	@After
 	public void cleanUp(){
 		controller = null;
